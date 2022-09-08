782d706f07ed3f41322771afe103d9bad5556119
fantoma37 <37fantoma@gmail.com>
2022-09-07 22:14:09 -0400

    Lab 0

---
+++ GItIntro.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "<h2>My Partner</h2>";
+        echo "</br>My partner's name is Benia Morgan. They are majoring in "
+        . "Software engineering and are taking the class because it's a "
+                . "requirement for their degree.";
+        echo "</br>They have taken Java, HTML, and are currently learning Swift as well.";
+        echo "</br>They don't have a ton of hobbies but likes to play The Sims, "
+        . "read, and hang with their little one.";
+        echo "</br>They are full time here at OCC & doing the Apple Developer "
+        . "Academy. So they're fully booked for right now.";
+        echo "</br>They're a super friendly and easy person to talk to. "
+        . "Even though this is an online class, they'd still love to connect with their classmates.";
+        ?>
+    </body>
+</html>
